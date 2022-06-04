1a2621225567a5d162ba860f125a025469a5fbf1
madhavnagpal <mr.madhavnagpal@gmail.com>
2022-06-04 11:55:35 +0530

    revamped cart context using useReducer

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useReducer } from "react";
 import { cloneDeep } from "../utils/helpers.utils";
 
 export const CartContext = createContext({
@@ -10,34 +10,95 @@ export const CartContext = createContext({
   deleteItemFromCart: () => {},
 });
 
-export const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState({});
-  const [cartCount, setCartCount] = useState(0);
-  const [cartTotal, setCartTotal] = useState(0);
+const initialState = {
+  cartItems: {},
+  cartCount: 0,
+  cartTotal: 0,
+};
 
-  useEffect(() => {
-    let count = 0;
-    let totalPrice = 0;
-    Object.values(cartItems).forEach(({ price, quantity }) => {
-      count += quantity;
-      totalPrice += quantity * price;
-    });
-    setCartCount(count);
-    setCartTotal(totalPrice);
-  }, [cartItems]);
+const CART_ACTION_TYPES = {
+  ADD_CART_ITEM: "ADD_CART_ITEM",
+  DECREMENT_CART_ITEM: "DECREMENT_CART_ITEM",
+  DELETE_CART_ITEM: "DELETE_CART_ITEM",
+};
+
+const cartReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case CART_ACTION_TYPES.ADD_CART_ITEM:
+      return addItem(state, payload);
+    case CART_ACTION_TYPES.DECREMENT_CART_ITEM:
+      return decrementItem(state, payload);
+    case CART_ACTION_TYPES.DELETE_CART_ITEM:
+      return deleteItem(state, payload);
+    default:
+      throw new Error(`unhandled type of ${type} in cartReducer`);
+  }
+};
+
+function addItem(prevState, item) {
+  const updatedCartItems = cloneDeep(prevState.cartItems);
+  if (updatedCartItems[item.id]) {
+    updatedCartItems[item.id].quantity += 1;
+  } else {
+    updatedCartItems[item.id] = { ...item, quantity: 1 };
+  }
+  return {
+    cartItems: updatedCartItems,
+    cartTotal: prevState.cartTotal + item.price,
+    cartCount: prevState.cartCount + 1,
+  };
+}
+
+function decrementItem(prevState, itemId) {
+  const updatedCartItems = cloneDeep(prevState.cartItems);
+  updatedCartItems[itemId].quantity -= 1;
+  const itemPrice = updatedCartItems[itemId].price;
+  if (!updatedCartItems[itemId].quantity) {
+    delete updatedCartItems[itemId];
+  }
+  return {
+    cartItems: updatedCartItems,
+    cartTotal: prevState.cartTotal - itemPrice,
+    cartCount: prevState.cartCount - 1,
+  };
+}
+
+function deleteItem(prevState, itemId) {
+  const updatedCartItems = cloneDeep(prevState.cartItems);
+  const itemPrice = updatedCartItems[itemId].price;
+  const itemQuantity = updatedCartItems[itemId].quantity;
+  delete updatedCartItems[itemId];
+  return {
+    cartItems: updatedCartItems,
+    cartTotal: prevState.cartTotal - itemPrice * itemQuantity,
+    cartCount: prevState.cartCount - itemQuantity,
+  };
+}
+
+export const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(cartReducer, initialState);
+  const { cartItems, cartCount, cartTotal } = state;
 
   const addItemToCart = (cartItemToAdd) => {
-    setCartItems(addCartItem(cartItems, cartItemToAdd));
+    dispatch({
+      type: CART_ACTION_TYPES.ADD_CART_ITEM,
+      payload: cartItemToAdd,
+    });
   };
 
   const decrementItemQuantity = (cartItemId) => {
-    setCartItems(decrementCartItemQuantity(cartItems, cartItemId));
+    dispatch({
+      type: CART_ACTION_TYPES.DECREMENT_CART_ITEM,
+      payload: cartItemId,
+    });
   };
 
   const deleteItemFromCart = (cartItemId) => {
-    const cartItemsCopy = cloneDeep(cartItems);
-    delete cartItemsCopy[cartItemId];
-    setCartItems(cartItemsCopy);
+    dispatch({
+      type: CART_ACTION_TYPES.DELETE_CART_ITEM,
+      payload: cartItemId,
+    });
   };
 
   const value = {
@@ -51,22 +112,3 @@ export const CartProvider = ({ children }) => {
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
-
-function addCartItem(cartItems, cartItemToAdd) {
-  const cartItemsCopy = cloneDeep(cartItems);
-  if (cartItemsCopy[cartItemToAdd.id]) {
-    cartItemsCopy[cartItemToAdd.id].quantity += 1;
-  } else {
-    cartItemsCopy[cartItemToAdd.id] = { ...cartItemToAdd, quantity: 1 };
-  }
-  return cartItemsCopy;
-}
-
-function decrementCartItemQuantity(cartItems, cartItemId) {
-  const cartItemsCopy = cloneDeep(cartItems);
-  cartItemsCopy[cartItemId].quantity -= 1;
-  if (!cartItemsCopy[cartItemId].quantity) {
-    delete cartItemsCopy[cartItemId];
-  }
-  return cartItemsCopy;
-}
