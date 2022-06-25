cb27baee585a1d7865eca71ace47866a16cc3b82
madhavnagpal <mr.madhavnagpal@gmail.com>
2022-06-25 13:44:21 +0530

    feat(redux-category): added reselect library and memoized categoryMap selector preventing extra re-renders

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,6 +1,12 @@
 /*All our reducers reacts to every action, since redux
   is singular store approach (single source of truth) */
 
+/*
+  Whenever any action fires and as long as reducer updates, it does not
+  matter which part of the reducer your Component is listening to,
+  every useSelector re-run (and whether it triggers a re-render in our component is upto how we code out the structure)
+*/
+
 import { combineReducers } from "redux";
 import { userReducer } from "./user/user.reducer";
 import { categoriesReducer } from "./categories/category.reducer";
